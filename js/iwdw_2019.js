7042f9e0df7be5dd1d8b5cbd8983f2926373fd77
zym2513023434 <2513023434@qq.com>
2019-01-18 15:29:12 +0800

    1-18

--- js/iwdw_2019.js
+++ js/iwdw_2019.js
@@ -1,7 +1,43 @@
 $(document).ready(function(){
+    //导航栏
+    var navbox='<div class="nav" id="smoothmenu1">\
+            <ul class="clearfix">\
+                <li class="selected home"><a href="index.html"><span>Home</span></a></li>\
+                <li class="program">\
+                    <a  href="#"><span>Program</span></a>\
+                    <ul class="submenu">\
+                        <li><a href="keynote.html">Keynote speakers</a></li>\
+                        <li><a href="program.html">Program</a></li>\
+                    </ul>\
+                </li>\
+                <li class="cfp">\
+                    <a  href="#"><span>CFP</span></a>\
+                    <ul class="submenu">\
+                        <li><a href="callPaper.html">Call for papers</a></li>\
+                        <li><a href="submission.html">Submission of papers</a></li>\
+                        <li><a href="paperRe.html">Paper Review</a></li>\
+                    </ul>\
+                </li>\
+                <li class="committees">\
+                    <a  href="Committee.html"><span>Committees</span></a>\
+                </li>\
+                <li class="location">\
+                    <a  href="#"><span>Location</span></a>\
+                    <ul class="submenu">\
+                        <li><a href="venueIn.html">Venue Information</a></li>\
+                        <li><a href="accormado.html">Accommodation</a></li>\
+                        <li><a href="transpor.html">Transportation</a></li>\
+                        <li><a href="sight.html">Sightseeing</a></li>\
+                    </ul>\
+                </li>\
+                <li class="registration">\
+                    <a  href="regist.html"><span>Registration</span></a>\
+                </li>\
+            </ul>\
+        </div>';
+
     //<!--图像集-->    
-    var theme_title='<div class="theme-title">\
-        <div class="logo">\
+    var theme_title='<div class="logo">\
             <a href="index.html"><img src="images/logo.png" /></a>\
         </div>\
         <div class="slider">\
@@ -17,18 +53,16 @@ $(document).ready(function(){
                 <a href="#">3</a>\
                 <a href="#">4</a>\
             </div>\
-        </div>\
-        </div>'
+        </div>';
+    var footer='<p>Copyright(c) IWDW 2019 Conference committees. All rights reserved.</p>';
+    $('.navbox').html(navbox);
     $('.theme-title').html(theme_title);
-
-
-
-
-
+    $('footer').html(footer);
 
 
     //导航栏
-    var elid = '.' + $('body').attr('id');
+    var elid = $('body').attr('id') || 'home';
+    elid = '.' + elid;
     $('.clearfix').children('.selected').removeClass('selected');    
     $('.clearfix').children(elid).addClass('selected');
 
